80b20d795abd50711ce690fd2db5bb71af629560
JustinVerkade <justin.n.verkade@gmail.com>
2024-11-14 20:31:09 +0100

    Added EmbeddedAdventOfCode to the general advent of code repo

---
+++ 2023/Debug/Core/Src/Drivers/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/Drivers/MDT0096AIH.c 
+
+OBJS += \
+./Core/Src/Drivers/MDT0096AIH.o 
+
+C_DEPS += \
+./Core/Src/Drivers/MDT0096AIH.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/Drivers/%.o Core/Src/Drivers/%.su Core/Src/Drivers/%.cyclo: ../Core/Src/Drivers/%.c Core/Src/Drivers/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F745xx -c -I../Core/Inc -I../Drivers/STM32F7xx_HAL_Driver/Inc -I../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F7xx/Include -I../Drivers/CMSIS/Include -I../FATFS/Target -I../FATFS/App -I../USB_DEVICE/App -I../USB_DEVICE/Target -I../USB_HOST/App -I../USB_HOST/Target -I../Middlewares/Third_Party/FatFs/src -I../Middlewares/ST/STM32_USB_Device_Library/Core/Inc -I../Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc -I../Middlewares/ST/STM32_USB_Host_Library/Core/Inc -I../Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core-2f-Src-2f-Drivers
+
+clean-Core-2f-Src-2f-Drivers:
+	-$(RM) ./Core/Src/Drivers/MDT0096AIH.cyclo ./Core/Src/Drivers/MDT0096AIH.d ./Core/Src/Drivers/MDT0096AIH.o ./Core/Src/Drivers/MDT0096AIH.su
+
+.PHONY: clean-Core-2f-Src-2f-Drivers
+
