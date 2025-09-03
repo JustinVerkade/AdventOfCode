4c2a7b49bc69175fa5312a4a816bd92a943fb141
JustinVerkade <justin.n.verkade@gmail.com>
2025-09-03 14:08:43 +0200

    Solved 2018 day 19

--- 2018/Day 19/part1.c
+++ 2018/Day 19/part1.c
@@ -28,222 +28,189 @@ enum Instructions_e {
     INST_EQRR,
 };
 
-typedef struct Test_t Test_t;
-struct Test_t {
-    int32_t before[4];
-    int32_t assembly[4];
-    int32_t after[4];
+typedef struct Instruction_t Instruction_t;
+struct Instruction_t {
+    Instructions_e type;
+    int8_t a;
+    int8_t b;
+    int8_t c;
 };
 
 //######################################################################################################################
 
-void inst_addr(int32_t registers[4], int32_t a, int32_t b, int32_t c)
+void inst_addr(int32_t registers[6], int32_t a, int32_t b, int32_t c)
 {
     registers[c] = registers[a] + registers[b];
 }
 
-void inst_addi(int32_t registers[4], int32_t a, int32_t b, int32_t c)
+void inst_addi(int32_t registers[6], int32_t a, int32_t b, int32_t c)
 {
     registers[c] = registers[a] + b;
 }
 
-void inst_mulr(int32_t registers[4], int32_t a, int32_t b, int32_t c)
+void inst_mulr(int32_t registers[6], int32_t a, int32_t b, int32_t c)
 {
     registers[c] = registers[a] * registers[b];
 }
 
-void inst_muli(int32_t registers[4], int32_t a, int32_t b, int32_t c)
+void inst_muli(int32_t registers[6], int32_t a, int32_t b, int32_t c)
 {
     registers[c] = registers[a] * b;
 }
 
-void inst_banr(int32_t registers[4], int32_t a, int32_t b, int32_t c)
+void inst_banr(int32_t registers[6], int32_t a, int32_t b, int32_t c)
 {
     registers[c] = registers[a] & registers[b];
 }
 
-void inst_bani(int32_t registers[4], int32_t a, int32_t b, int32_t c)
+void inst_bani(int32_t registers[6], int32_t a, int32_t b, int32_t c)
 {
     registers[c] = registers[a] & b;
 }
 
-void inst_borr(int32_t registers[4], int32_t a, int32_t b, int32_t c)
+void inst_borr(int32_t registers[6], int32_t a, int32_t b, int32_t c)
 {
     registers[c] = registers[a] | registers[b];
 }
 
-void inst_bori(int32_t registers[4], int32_t a, int32_t b, int32_t c)
+void inst_bori(int32_t registers[6], int32_t a, int32_t b, int32_t c)
 {
     registers[c] = registers[a] | b;
 }
 
-void inst_setr(int32_t registers[4], int32_t a, int32_t b, int32_t c)
+void inst_setr(int32_t registers[6], int32_t a, int32_t b, int32_t c)
 {
     registers[c] = registers[a];
 }
 
-void inst_seti(int32_t registers[4], int32_t a, int32_t b, int32_t c)
+void inst_seti(int32_t registers[6], int32_t a, int32_t b, int32_t c)
 {
     registers[c] = a;
 }
 
-void inst_gtir(int32_t registers[4], int32_t a, int32_t b, int32_t c)
+void inst_gtir(int32_t registers[6], int32_t a, int32_t b, int32_t c)
 {
     registers[c] = a > registers[b] ? 1 : 0;
 }
 
-void inst_gtri(int32_t registers[4], int32_t a, int32_t b, int32_t c)
+void inst_gtri(int32_t registers[6], int32_t a, int32_t b, int32_t c)
 {
     registers[c] = registers[a] > b ? 1 : 0;
 }
 
-void inst_gtrr(int32_t registers[4], int32_t a, int32_t b, int32_t c)
+void inst_gtrr(int32_t registers[6], int32_t a, int32_t b, int32_t c)
 {
     registers[c] = registers[a] > registers[b] ? 1 : 0;
 }
 
-void inst_eqir(int32_t registers[4], int32_t a, int32_t b, int32_t c)
+void inst_eqir(int32_t registers[6], int32_t a, int32_t b, int32_t c)
 {
     registers[c] = a == registers[b] ? 1 : 0;
 }
 
-void inst_eqri(int32_t registers[4], int32_t a, int32_t b, int32_t c)
+void inst_eqri(int32_t registers[6], int32_t a, int32_t b, int32_t c)
 {
     registers[c] = registers[a] == b ? 1 : 0;
 }
 
-void inst_eqrr(int32_t registers[4], int32_t a, int32_t b, int32_t c)
+void inst_eqrr(int32_t registers[6], int32_t a, int32_t b, int32_t c)
 {
     registers[c] = registers[a] == registers[b] ? 1 : 0;
 }
 
-void(*instructions[])(int32_t registers[4], int32_t a, int32_t b, int32_t c) = {
+void(*instructions[])(int32_t registers[6], int32_t a, int32_t b, int32_t c) = {
     inst_addr, inst_addi, inst_mulr, inst_muli, inst_banr, inst_bani, inst_borr, inst_bori,
     inst_setr, inst_seti, inst_gtir, inst_gtri, inst_gtrr, inst_eqir, inst_eqri, inst_eqrr};
 const int32_t instruction_count = 16;
 
-void mask_print(int32_t value)
+void print_state(int32_t ip_index, int32_t registers[6])
 {
-    printf("Mask: ");
-    for (int32_t i=15; i>=0; i--) {
-        if (value & (1<<i)) {
-            printf("1");
-        } else {
-            printf("0");
-        }
-    }
-    printf("\n");
+    printf("ip=%d [%d, %d, %d, %d, %d, %d]\n",
+        registers[ip_index], registers[0], registers[1], registers[2],
+        registers[3], registers[4], registers[5]);
 }
 
 int solve_puzzle(char* file_name)
 {
-    // fetch test input data
     FILE* p_file = fopen(file_name, "r");
     if (p_file == NULL) {
         printf("Failed to open file!\n");
         return 1;
     }
 
-    Test_t tests[2000];
-    int32_t tests_size = sizeof(tests) / sizeof(Test_t);
-    int32_t tests_count = 0;
+    int32_t pointer_index = 0;
+    int32_t registers[6] = {0};
+    char instruction[8] = {0};
+    int32_t a = 0;
+    int32_t b = 0;
+    int32_t c = 0;
+    fscanf(p_file, "#ip %d\n", &pointer_index);
 
-    for (; tests_count<tests_size; tests_count++) {
-        int8_t ret = 0;
-        ret = fscanf(p_file, "Before: [%d,%d,%d,%d]\n",
-            &tests[tests_count].before[0],
-            &tests[tests_count].before[1],
-            &tests[tests_count].before[2],
-            &tests[tests_count].before[3]);
-        if (ret != 4) {
-            break;
+    Instruction_t buffer[2000];
+    int32_t buffer_size = sizeof(buffer) / sizeof(Instruction_t);
+    int32_t buffer_count = 0;
+
+    while (fscanf(p_file, "%s %d %d %d\n", instruction, &a, &b, &c) > 0) {
+        Instruction_t* inst = &buffer[buffer_count];
+        inst->a = a;
+        inst->b = b;
+        inst->c = c;
+        if (!memcmp(instruction, "addr", 4)) {
+            inst->type = INST_ADDR;
+        } else if (!memcmp(instruction, "addi", 4)) {
+            inst->type = INST_ADDI;
+        } else if (!memcmp(instruction, "mulr", 4)) {
+            inst->type = INST_MULR;
+        } else if (!memcmp(instruction, "muli", 4)) {
+            inst->type = INST_MULI;
+        } else if (!memcmp(instruction, "banr", 4)) {
+            inst->type = INST_BANR;
+        } else if (!memcmp(instruction, "bani", 4)) {
+            inst->type = INST_BANI;
+        } else if (!memcmp(instruction, "borr", 4)) {
+            inst->type = INST_BORR;
+        } else if (!memcmp(instruction, "bori", 4)) {
+            inst->type = INST_BORI;
+        } else if (!memcmp(instruction, "setr", 4)) {
+            inst->type = INST_SETR;
+        } else if (!memcmp(instruction, "seti", 4)) {
+            inst->type = INST_SETI;
+        } else if (!memcmp(instruction, "gtir", 4)) {
+            inst->type = INST_GTIR;
+        } else if (!memcmp(instruction, "gtri", 4)) {
+            inst->type = INST_GTRI;
+        } else if (!memcmp(instruction, "gtrr", 4)) {
+            inst->type = INST_GTRR;
+        } else if (!memcmp(instruction, "eqir", 4)) {
+            inst->type = INST_EQIR;
+        } else if (!memcmp(instruction, "eqri", 4)) {
+            inst->type = INST_EQRI;
+        } else if (!memcmp(instruction, "eqrr", 4)) {
+            inst->type = INST_EQRR;
+        } else {
+            printf("Unknown register type!\n");
+            fclose(p_file);
+            return 1;
         }
-        ret = fscanf(p_file, "%d %d %d %d\n", 
-            &tests[tests_count].assembly[0], 
-            &tests[tests_count].assembly[1],
-            &tests[tests_count].assembly[2],
-            &tests[tests_count].assembly[3]);
-        if (ret != 4) {
-            break;
-        }
-        ret = fscanf(p_file, "After:  [%d,%d,%d,%d]\n",
-            &tests[tests_count].after[0],
-            &tests[tests_count].after[1],
-            &tests[tests_count].after[2],
-            &tests[tests_count].after[3]);
-        if (ret != 4) {
-            break;
+        buffer_count++;
+        if (buffer_count == buffer_size) {
+            printf("Buffer size to small!\n");
+            fclose(p_file);
+            return 1;
         }
     }
+    fclose(p_file);
 
-    // create initial masks.
-    int32_t masks[16] = {0};
-    int32_t masks_size = sizeof(masks) / sizeof(int32_t);
-    memset(masks, 0xFF, sizeof(int32_t) * masks_size);
-
-    for (int32_t test_id=0; test_id<tests_count; test_id++) {
-        Test_t* test = &tests[test_id];
-        int32_t sub_counter = 0;
-        for (int32_t inst_id=0; inst_id<instruction_count; inst_id++) {
-            int32_t registers[4] = {test->before[0], test->before[1], test->before[2], test->before[3]};
-            int32_t instruction = test->assembly[0];
-            int32_t a = test->assembly[1];
-            int32_t b = test->assembly[2];
-            int32_t c = test->assembly[3];
-            instructions[inst_id](registers, a, b, c);
-            if(memcmp(registers, test->after, sizeof(int32_t) * 4) != 0){
-                masks[instruction] &= ~(1<<inst_id);
-            }
-        }
-    }
-
-    // solve masks per op code.
-    for (;;) {
-        int32_t solved = 0;
-        for (int32_t mask=0; mask<masks_size; mask++) {
-            int32_t bit_id = 0;
-            int32_t counter = 0;
-            for (int32_t bit=0; bit<masks_size; bit++) {
-                if (masks[mask] & (1<<bit)) {
-                    bit_id = bit;
-                    counter++;
-                }
-            }
-            if (counter == 1) {
-                solved++;
-                for (int32_t clear=0; clear<masks_size; clear++) {
-                    if (clear == mask) {
-                        continue;
-                    }
-                    masks[clear] &= ~(1<<bit_id);
-                }
-            }
-        }
-        if (solved == masks_size) {
-            break;
-        }
-    }
-
-    // execute instructions
-    int32_t registers[6] = {0, 0, 0, 0};
-    char buffer[64] = {0};
-    while (fgets(buffer, 64, p_file)) {
-        int32_t instruction = 0;
-        int32_t a = 0;
-        int32_t b = 0;
-        int32_t c = 0;
-        sscanf(buffer, "%d %d %d %d\n", &instruction, &a, &b, &c);
-        int32_t mask_id = 0;
-        for (;mask_id<masks_size; mask_id++) {
-            if (masks[instruction] & (1<<mask_id)) {
-                break;
-            }
-        }
-        instructions[mask_id](registers, a, b, c);
+    while (registers[pointer_index] >= 0 && registers[pointer_index] < buffer_count) {
+        Instruction_t* inst = &buffer[registers[pointer_index]];
+        instructions[inst->type](registers, inst->a, inst->b, inst->c);
+        registers[pointer_index]++;
     }
 
     int32_t answer = registers[0];
     printf("Answer: %d\n", answer);
+
+    return 0;
 }
 
 //######################################################################################################################
--- 2018/Day 19/part2.c
+++ 2018/Day 19/part2.c
@@ -8,13 +8,188 @@
 //######################################################################################################################
 // type defines
 
+typedef enum Instructions_e Instructions_e;
+enum Instructions_e {
+    INST_ADDR=0,
+    INST_ADDI,
+    INST_MULR,
+    INST_MULI,
+    INST_BANR,
+    INST_BANI,
+    INST_BORR,
+    INST_BORI,
+    INST_SETR,
+    INST_SETI,
+    INST_GTIR,
+    INST_GTRI,
+    INST_GTRR,
+    INST_EQIR,
+    INST_EQRI,
+    INST_EQRR,
+};
 
+typedef struct Instruction_t Instruction_t;
+struct Instruction_t {
+    Instructions_e type;
+    int8_t a;
+    int8_t b;
+    int8_t c;
+};
 
 //######################################################################################################################
 
+void inst_addr(int32_t registers[6], int32_t a, int32_t b, int32_t c)
+{
+    registers[c] = registers[a] + registers[b];
+}
+
+void inst_addi(int32_t registers[6], int32_t a, int32_t b, int32_t c)
+{
+    registers[c] = registers[a] + b;
+}
+
+void inst_mulr(int32_t registers[6], int32_t a, int32_t b, int32_t c)
+{
+    registers[c] = registers[a] * registers[b];
+}
+
+void inst_muli(int32_t registers[6], int32_t a, int32_t b, int32_t c)
+{
+    registers[c] = registers[a] * b;
+}
+
+void inst_banr(int32_t registers[6], int32_t a, int32_t b, int32_t c)
+{
+    registers[c] = registers[a] & registers[b];
+}
+
+void inst_bani(int32_t registers[6], int32_t a, int32_t b, int32_t c)
+{
+    registers[c] = registers[a] & b;
+}
+
+void inst_borr(int32_t registers[6], int32_t a, int32_t b, int32_t c)
+{
+    registers[c] = registers[a] | registers[b];
+}
+
+void inst_bori(int32_t registers[6], int32_t a, int32_t b, int32_t c)
+{
+    registers[c] = registers[a] | b;
+}
+
+void inst_setr(int32_t registers[6], int32_t a, int32_t b, int32_t c)
+{
+    registers[c] = registers[a];
+}
+
+void inst_seti(int32_t registers[6], int32_t a, int32_t b, int32_t c)
+{
+    registers[c] = a;
+}
+
+void inst_gtir(int32_t registers[6], int32_t a, int32_t b, int32_t c)
+{
+    registers[c] = a > registers[b] ? 1 : 0;
+}
+
+void inst_gtri(int32_t registers[6], int32_t a, int32_t b, int32_t c)
+{
+    registers[c] = registers[a] > b ? 1 : 0;
+}
+
+void inst_gtrr(int32_t registers[6], int32_t a, int32_t b, int32_t c)
+{
+    registers[c] = registers[a] > registers[b] ? 1 : 0;
+}
+
+void inst_eqir(int32_t registers[6], int32_t a, int32_t b, int32_t c)
+{
+    registers[c] = a == registers[b] ? 1 : 0;
+}
+
+void inst_eqri(int32_t registers[6], int32_t a, int32_t b, int32_t c)
+{
+    registers[c] = registers[a] == b ? 1 : 0;
+}
+
+void inst_eqrr(int32_t registers[6], int32_t a, int32_t b, int32_t c)
+{
+    registers[c] = registers[a] == registers[b] ? 1 : 0;
+}
+
+void(*instructions[])(int32_t registers[6], int32_t a, int32_t b, int32_t c) = {
+    inst_addr, inst_addi, inst_mulr, inst_muli, inst_banr, inst_bani, inst_borr, inst_bori,
+    inst_setr, inst_seti, inst_gtir, inst_gtri, inst_gtrr, inst_eqir, inst_eqri, inst_eqrr};
+const int32_t instruction_count = 16;
+
+void print_state(int32_t ip_index, int32_t registers[6])
+{
+    printf("ip=%2d [%12d, %12d, %12d, %12d, %12d, %12d]\n",
+        registers[ip_index], registers[0], registers[1], registers[2],
+        registers[3], registers[4], registers[5]);
+}
+
 int solve_puzzle(char* file_name)
 {
-    printf("File: %s\n", file_name);
+
+    int32_t a = 1;
+    int32_t c = 0;
+    int32_t d = 0;
+    int32_t e = 0;
+    int32_t f = 0;
+
+    e += 2;
+    e = e * e;
+    e *= 19;
+    e *= 11;
+    c += 5;
+    c *= 22;
+    c += 12;
+    e += c;
+    if (a) {
+        c = 27;
+        c *= 28;
+        c += 29;
+        c *= 30;
+        c *= 14;
+        c *= 32;
+        e += c;
+        a = 0;
+    }
+
+    // reverse engineered program
+    // f = 1;
+    // for (;;) {
+    //     d = 1;
+    //     for (;;) {
+    //         if (f * d == e) {
+    //             a += f;
+    //             printf("[%d %d, %d, %d, %d]\n", a, c, d, e, f);
+    //         }
+    //         d += 1;
+    //         if (d > e) {
+    //             f += 1;
+    //             if (f > e) {
+    //                 goto GOTO_16;
+    //             }
+    //             break;
+    //         }
+    //     }
+    // }
+    // GOTO_16:
+    // int32_t answer = a;
+
+    // optimized
+    int32_t answer = 0;
+    for (int32_t i=1; i<=e; i++) {
+        if (e % i == 0) {
+            answer += i;
+        }
+    }
+
+    printf("Answer: %d\n", answer);
+    return 0;
 }
 
 //######################################################################################################################
